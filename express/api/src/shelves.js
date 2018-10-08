4c2ccf531708fbb6faf5a1146e2090605647016a
Mario Carl <mariocarl@gmail.com>
2018-10-08 09:25:22 -0500

    added shelves.js

---
+++ express/api/src/shelves.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cassClient = require('./services/cassandra-client');
+
+const router = express();
+
+//get user shelf
+
+
+
+//add shelf             *
+//remove shelf          *
+//edit shelf            *
+//add description       *
+//remove description    *
+//edit descrition       *
+//add item              *
+//remove item           *
+//edit item             *
+//add comment           *
+//remove comment        *
+//edit comment          *
+
+module.exports = router;
