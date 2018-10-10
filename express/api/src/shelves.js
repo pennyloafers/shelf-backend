00e3e269f2e0061f77b9c4c124d6459599c8f391
Mario Carl <mariocarl@gmail.com>
2018-10-10 11:29:58 -0500

    Added query template

--- express/api/src/shelves.js
+++ express/api/src/shelves.js
@@ -3,19 +3,41 @@ const cassClient = require('./services/cassandra-client');
 
 const router = express();
 
+router.post('/getUserShelves', getUserShelves);
 //get user shelf
+function getUserShelves(req,res) {
+    console.log(req)
+    //Build query: ? can be filled later
+    let query = "SELECT * FROM shelves WHERE username = ?";
+
+    //Prepare parameters for ?'s: must be an array
+    const params = [req.user];
+ 
+    //call client with query & params: {prepare: true}
+    cassClient.execute(query, params, { prepare: true })
+    .then(result => {
+        console.log(result);
+        //do something with 'result' 
+        //send results with res back to caller.
+        res.send({ /*data*/ });
+ 
+    })
+    .catch (error => {
+        console.log(error.message);
+        res.send({ /*error*/ });
+ 
+    })
+}
 
 
-
-//add shelf             *
+//new shelf             *
 //remove shelf          *
 //edit shelf            *
-//add description       *
-//remove description    *
-//edit descrition       *
 //add item              *
 //remove item           *
 //edit item             *
+//add item description       *
+//remove item description    *
 //add comment           *
 //remove comment        *
 //edit comment          *
