c29a809b6eb6b4953c287996b6d23d90633e9f8c
jdaughd2 <joseph.daughdrill@usm.edu>
2018-10-15 05:34:35 -0500

    Added commands to restart docker images in init.sh

--- cassandra/init.sh
+++ cassandra/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#Uncomment the lines of the commands you wish to run
+# Uncomment the lines of the commands you wish to run
 
-#Requires package jq to read secrets.json
-#Install jq package if not installed
+# Requires package jq to read secrets.json
+# Install jq package if not installed
 if [ $(dpkg-query -W -f='${Status}' jq 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
 	echo "Installing jq..."
@@ -13,6 +13,11 @@ fi
 # Copy necessary files to container
 #docker cp ./settings/cassandra.yaml cass:/etc/cassandra/
 #docker cp ./init cass:/
+
+# Restart the docker images
+#docker-compose stop cassandra
+#docker-compose stop express
+#docker-compose up -d
 
 # Look up passwords
 ROOT=$(jq -r '.cassRoot' "../express/api/secret.json")
